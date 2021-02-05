9c41bf23e914701d964f95daa580f64b4a0f0f31
Muhammad Fauzan <fgaming386@gmail.com>
2021-02-05 17:56:46 +0700

    Forcing update

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to install?
+- git clone https://github.com/fauzanw/indosec.id
+- yarn install
+- yarn run serve
+- Open this file in your browser
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#9D5BFE',
+        primary2: '#822DFF',
+        dark: '#233051',
+        'dark-100': '#F8F5F5',
+        'dark-200': '#908EAB',
+        'dark-300': '#F3F2F2',
+        secondary: '#E9EBF1'
+      }
+    },
+    fontFamily: {
+      mrwb: ["Merriweather-Black"],
+      mrwbold: ["Merriweather-Bold"],
+      mrwl: ["Merriweather-Light"],
+      mrwr: ["Merriweather-Regular"],
+      pb: ["Poppins-Bold"],
+      pm: ["Poppins-Medium"],
+      pr: ["Poppins-Regular"],
+    }
+  }
+}
